2ba860f75c23dea020918c4302683ffd5cb20752
julyluna10 <julyluna10@gmail.com>
2023-12-18 07:42:08 -0300

    Inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -34,40 +34,38 @@ const Inicio = () => {
     return (
         <div className='inicio'>
             <section className='inicioSeccion1'>
-                <div className='sombreado'>
-                    <h1>Canch<span className='inicioColoreadoApp'>App</span></h1>
-                    <p>Somos una aplicación dedicada a facilitar la reserva de canchas deportivas.</p>
+                <div className='sombreado text-left'>
+                    <h1 className="m-0">Canch<span className='inicioColoreadoApp'>App</span></h1>
+                    <p className="inicioSeccion1Texto">El fútbol a un click de distancia.</p>
                 </div>
             </section>
 
             <section className='inicioSeccion2'>
-                <h2 className='inicioSeccionTitulo'>Conócenos</h2>
-                <div className=''>
-                    <div data-aos="fade-up" className='inicioSeccion2TextoContainer text-left'>
-                        <h4>¿Quiénes somos?</h4>
-                        <p>En CanchApp, somos la plataforma líder que simplifica la reserva de canchas deportivas. Con pasión por el deporte y un compromiso inquebrantable, conectamos de manera efectiva a los dueños de complejos con los apasionados jugadores.</p>
-                    </div>
-                    <div data-aos="fade-up" className='inicioSeccion2TextoContainer text-left'>
-                        <h4>Objetivo</h4>
-                        <p>Nuestro compromiso es facilitar una experiencia fluida y eficiente, conectando dueños de complejos y jugadores con soluciones innovadoras que enriquezcan la pasión por el deporte, la camaradería y la comunicación.</p>
-                    </div>
-                    <div data-aos="fade-up" className='inicioSeccion2TextoContainer text-left'>
-                        <h4>Valores</h4>
-                        <p>
-                        Transparencia: Fomentamos la apertura y honestidad en todas nuestras interacciones, construyendo relaciones de confianza.
-                        <br />
-                        Confiabilidad: Ofrecemos una plataforma sólida y segura en la que tanto dueños como jugadores pueden depender para gestionar reservas de manera eficiente.
-                        </p>
+                <div className="inicioSeccion2ConocenosCentrado">
+                    <div className='inicioSeccion2Conocenos'>
+                        <div className="inicioSeccion2ConocenosTexto">
+                            <h2 className='inicioSeccionTitulo'>Conócenos</h2>
+
+                            <p data-aos="fade-up" className="text-left">En CanchApp, no solo creamos una aplicación; construimos un hogar para la comunidad apasionada de jugadores de fútbol 5. Nuestra historia se teje con la pasión por el deporte, la innovación y el deseo de hacer que cada momento en la cancha sea inolvidable. </p>
+                            <p data-aos="fade-up" className="text-left">Somos un equipo diverso, fusionando habilidades técnicas con un amor compartido por el fútbol. Nos inspira la idea de simplificar la organización de partidos, conectar a jugadores de todas partes y brindarte una experiencia futbolística sin complicaciones.</p>
+                            <p data-aos="fade-up" className="text-left">Creemos en la comunidad y en el poder de unirse a través del deporte. CanchApp no solo te ofrece una plataforma, sino un lugar donde puedes compartir experiencias, retos y éxitos con otros amantes del fútbol 5. Nos esforzamos por ser más que una aplicación; somos una familia virtual que comparte la misma pasión por el juego.</p>
+
+                        </div>
+                        <div className="inicioSeccion2ConocenosImagenContainer">
+                            <img src="/images/inicio/conocenos.webp" alt="" className="inicioSeccion2ConocenosImagen"/>
+                        </div>
                     </div>
                 </div>
+                
+                
             </section>
             
             <section className='inicioSeccion3'>
-                <h2 className='inicioSeccionTitulo'>Dueños</h2>
-                <div className='flex justify-around items-stretch flex-wrap gap-20'>
+                <h2 className='inicioSeccionTitulo'>Administradores de canchas</h2>
+                <div className='flex justify-around items-stretch flex-wrap inicioSeccionCardsContainer'>
                     <div data-aos="fade-up" data-aos-delay="50">
                         <CardInicio
-                        imagenSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrVGyZIpSiyAcGPcfo1jzHWNXMzxmTV8aYmgDBCi1-A&s"
+                        imagenSrc="/images/inicio/1.webp"
                         titulo="Registra tu complejo"
                         descripcion="Pon tu complejo en el mapa y hazlo visible para jugadores de todas partes. Publica información sobre tus canchas, ubicación y horarios."
                         />
@@ -75,7 +73,7 @@ const Inicio = () => {
                     
                     <div data-aos="fade-up" data-aos-delay="100">
                         <CardInicio
-                        imagenSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrVGyZIpSiyAcGPcfo1jzHWNXMzxmTV8aYmgDBCi1-A&s"
+                        imagenSrc="/images/inicio/2.webp"
                         titulo="Administra tus horarios"
                         descripcion="Pon tu complejo en el mapa y hazlo visible para jugadores de todas partes. Publica información sobre tus canchas, ubicación y horarios."
                         />
@@ -83,24 +81,24 @@ const Inicio = () => {
                     
                     <div data-aos="fade-up" data-aos-delay="150">
                         <CardInicio
-                        imagenSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrVGyZIpSiyAcGPcfo1jzHWNXMzxmTV8aYmgDBCi1-A&s"
+                        imagenSrc="/images/inicio/3.webp"
                         titulo="Promociona tu complejo"
                         descripcion="Gana visibilidad y atrae a más jugadores promocionando tus instalaciones y servicios especiales."
                         />
                     </div>
                 </div>
-                <div>
-                    <h3>Registra tu complejo y forma parte de nuestra comunidad</h3>
-                    <p>No pierdas la oportunidad de dar a conocer tu complejo y llegar a nuevos clientes. Únete a nuestra plataforma y disfruta de los beneficios de formar parte de una comunidad deportiva en crecimiento.</p>
+                <div className="inicioSeccionBanner">
+                    <h3 className="inicioSeccionBannerTitulo">Registra tu complejo y forma parte de nuestra comunidad</h3>
+                    <p className="inicioSeccionBannerTexto">No pierdas la oportunidad de dar a conocer tu complejo y llegar a nuevos clientes. Únete a nuestra plataforma y disfruta de los beneficios de formar parte de una comunidad deportiva en crecimiento.</p>
                 </div>
                 </section>
 
                 <section className='inicioSeccion4'>
                     <h2 className='inicioSeccionTitulo'>Jugadores</h2>
-                    <div className='flex justify-around flex-wrap gap-20 items-stretch'>
+                    <div className='flex justify-around items-stretch flex-wrap inicioSeccionCardsContainer'>
                         <div data-aos="fade-up" data-aos-delay="50">
                             <CardInicio
-                            imagenSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrVGyZIpSiyAcGPcfo1jzHWNXMzxmTV8aYmgDBCi1-A&s"
+                            imagenSrc="/images/inicio/4.webp"
                             titulo="Busca y alquila canchas"
                             descripcion="Encuentra las canchas disponibles en tu zona y verifica la disponibilidad de las canchas en los complejos disponibles."
                             />
@@ -108,7 +106,7 @@ const Inicio = () => {
                         
                         <div data-aos="fade-up" data-aos-delay="100">
                             <CardInicio
-                            imagenSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrVGyZIpSiyAcGPcfo1jzHWNXMzxmTV8aYmgDBCi1-A&s"
+                            imagenSrc="/images/inicio/5.webp"
                             titulo="Crea y únete a equipos"
                             descripcion="Forma parte de una comunidad deportiva, crea tu propio equipo o únete a uno existente para participar en torneos y partidos amistosos."
                             />
@@ -116,17 +114,16 @@ const Inicio = () => {
                         
                         <div data-aos="fade-up" data-aos-delay="150">
                             <CardInicio
-                            imagenSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrVGyZIpSiyAcGPcfo1jzHWNXMzxmTV8aYmgDBCi1-A&s"
+                            imagenSrc="/images/inicio/6.webp"
                             titulo="Consulta y administra reservas"
                             descripcion="Mantente al tanto de tus reservas y adminístralas fácilmente. Accede a tu historial y organiza tus partidos de manera eficiente."
                             />
                         </div>
                         
                     </div>
-
-                    <div className='inicioBannerRegistrarse'>
-                        <h3 className='inicioBannerRegistrarseTitulo'>Únete a nuestra comunidad deportiva como jugador</h3>
-                        <p className='inicioBannerRegistrarseTexto m-0'>Explora nuevas oportunidades al unirte a CanchApp. Descubre canchas cercanas, forma equipos, y participa en vibrantes eventos deportivos. ¡Conéctate con otros apasionados del deporte y lleva tu experiencia a otro nivel!</p>
+                    <div className='inicioSeccionBanner'>
+                        <h3 className='inicioSeccionBannerTitulo'>Únete a nuestra comunidad deportiva como jugador</h3>
+                        <p className='inicioSeccionBannerTexto'>Explora nuevas oportunidades al unirte a CanchApp. Descubre canchas cercanas, forma equipos, y participa en vibrantes eventos deportivos. ¡Conéctate con otros apasionados del deporte y lleva tu experiencia a otro nivel!</p>
                         <button>Registrarse</button>
                     </div>
                 </section>
